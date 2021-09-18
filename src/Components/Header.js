6b9c3f56ff2b280e35edf8375e8ab16fc5805bd2
Jimmy Li <lijjimmy@gmail.com>
2021-09-17 23:09:03 -0400

    updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,26 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 
-const Header = ({ title}) => {
-
-    const onClick = (e) => {
+const Header = ({title}) => {
+    const onClick = () => {
         console.log('Click')
     }
 
     return (
-        <header className ='header'>
-            <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+        <header className='header'>
+            <h1 >Hello {title}</h1>
+            <Button color='green' onClick={onClick} text='Hello'/>
         </header>
     )
 }
 
+// const headingStyle = {
+//     color: 'white',
+//     backgroundColor: 'blue'
+// }
+
 Header.defaultProps = {
-    title: 'Task Tracker',
+    title: "Andrea Caicedo",
 }
 
 Header.propTypes = {

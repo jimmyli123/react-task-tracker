c414438ebd191a4ea8070aa869a1bd6680cdcc68
Jimmy Li <lijjimmy@gmail.com>
2021-09-19 18:51:29 -0400

    updating to latest

--- src/App.js
+++ src/App.js
@@ -5,18 +5,7 @@ import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
-    {
-      id: 11,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2:30pm',
-      reminder: 'true'
-    },
-    {
-      id:32,
-      text: 'Meeting at School',
-      day: 'Feb 6th at 1:30pm',
-      reminder: true,
-    }
+    
   ])
 
   // const test = (isd) => {
@@ -42,7 +31,8 @@ function App() {
     <div className="container">
       <Header/>
       <AddTask onAdd={addTask}/>
-      <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+      {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No Tasks To Show"}
+      
     </div>
   );
 }
